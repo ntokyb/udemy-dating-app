df20f449abd7c83c709ffdb2b6779a005ba97f32
Ntokozo Banda <ntokozo.banda@dimensiondata.com>
2022-01-20 16:01:21 +0200

    certificates

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent  implements OnInit {
+  title = 'Dasso';
+  users: any;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.users = response;
+      console.log('The users response', this.users);
+    }, error => {
+      console.log(error);
+    });
+  }
 }
